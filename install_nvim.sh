5a468797129313453023a95a07ed1743944df4d5
Dan Collins <dcollinsn@gmail.com>
2026-02-05 16:11:01 -0500

    Use subshell when setting up nvim to return to this directory and copy the config

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd /tmp
-rm -rf neovim
-git clone https://github.com/neovim/neovim
-cd neovim
-git checkout stable
-make CMAKE_BUILD_TYPE=release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/opt/nvim"
-sudo make install
+(
+    cd /tmp
+    rm -rf neovim
+    git clone https://github.com/neovim/neovim
+    cd neovim
+    git checkout stable
+    make CMAKE_BUILD_TYPE=release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/opt/nvim"
+    sudo make install
+)
 
 cp -R .config-nvim ~/.config/nvim
 
@@ -16,5 +18,5 @@ fi
 
 if [[ "$PATH" != *"/opt/nvim/bin"* ]]; then
     echo 'export PATH="$PATH:/opt/nvim/bin"' >> ~/.bashrc
-    echo "Make sure you run `source ~/.bashrc`"
+    echo "Make sure you run 'source ~/.bashrc'"
 fi
