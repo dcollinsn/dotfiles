50e24bb1a4f5917438ac6c67b5e7c25caeef0d45
Dan Collins <dcollins@ariadne>
2026-07-30 16:54:36 -0400

    manjaro: automatic TZ sync on connectivity change

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -10,3 +10,7 @@ nvm install node
 
 cp zshrc ~/.zshrc
 cp p10k.zsh ~/.p10k.zsh
+
+sudo cp timezone /etc/NetworkManager/dispatcher.d/09-timezone
+sudo chmod +x /etc/NetworkManager/dispatcher.d/09-timezone
+sudo systemctl enable ntpdate
